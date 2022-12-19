fa688e380d87186a647546aaa187fd583d05d634
dika <adikaridlotaqwin@gmail.com>
2022-12-19 09:38:46 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projek-belajar',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
